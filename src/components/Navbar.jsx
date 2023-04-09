ff45886f8e72f38234074deb725bff3029036d06
MYXF90 <mysteryxfiles90@gmail.com>
2023-04-09 18:45:45 +0300

    test

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
 
       <ul className="list-none sm:flex hidden justify-end items-center flex-1">
         {navLinks.map((nav, index) => (
-          <li
+          <li 
             key={nav.id}
             className={`font-poppins font-normal cursor-pointer text-[16px] ${active === nav.title ? "text-white" : "text-dimWhite"
               } ${index === navLinks.length - 1 ? "mr-0" : "mr-10"}`}
